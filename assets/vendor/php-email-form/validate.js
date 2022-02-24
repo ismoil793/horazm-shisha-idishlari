1d38db577de2b4a0237ef85bc4a55ef06a71bea0
ismoil793 <ismoil.793@gmail.com>
2022-02-24 21:41:55 +0500

    fix telegram chat id

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -8,16 +8,16 @@
 
   let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
+  forms.forEach(function (e) {
+    e.addEventListener('submit', function (event) {
       event.preventDefault();
 
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
+
+      if (!action) {
         displayError(thisForm, 'The form action property is not set!')
         return;
       }
@@ -25,18 +25,18 @@
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-      let formData = new FormData( thisForm );
+      let formData = new FormData(thisForm);
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
             try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+                })
+            } catch (error) {
               displayError(thisForm, error)
             }
           });
@@ -53,29 +53,29 @@
 
 
     var token = "5134193514:AAHfjaWuP74G-7X4zBSH-apNQ4FfHRMgCS0"
-    var chat_id = "-636137838"
+    var chat_id = "-1001757096693"
     var title = document.getElementById("subject").value;
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var body = document.getElementById("message").value;
     var message = "От: " + name + "%0AEmail: " + email + "%0A%0A" + "Тема: " + title + "%0A%0A" + body;
-    var url = "https://api.telegram.org/bot" + token + "/sendMessage?text=" + message + "&chat_id=" + chat_id;    
+    var url = "https://api.telegram.org/bot" + token + "/sendMessage?text=" + message + "&chat_id=" + chat_id;
 
     fetch(url, {
       method: 'POST'
     })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.ok) {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, "Что-то пошло не так...");
-    });
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (data.ok) {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+        } else {
+          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, "Что-то пошло не так...");
+      });
   }
 
   function displayError(thisForm, error) {
